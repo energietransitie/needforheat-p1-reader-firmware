a6a4a3d1492d632f127ce057d209e0be0ca6dcae
Joël <joel-weg@outlook.com>
2023-06-09 14:56:40 +0200

    deviceTime function added

---
+++ include/timestampTest.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
+#include <esp_system.h>
+#include <esp_log.h>
+#include <esp_err.h>
+#include <iostream> 
+
+#include <scheduler.hpp>
+
+#include <assert.h>
+
+/**
+		 * Run trough all the available test cases
+		 */
+void parsedTimestampsTests();
