f9868910cf8844173a14d971d8004b55dc4abe13
hterhofte <h.ter.hofte@windesheim.nl>
2023-06-11 02:14:43 +0200

    added units and improved naming to debug baudrate detection

--- src/abr.c
+++ src/abr.c
@@ -3,15 +3,15 @@
 
 #define INPUT_PIN 16
 #define bufferSize 20
-#define threshold_us ((1e6 / 9600 + 1e6 / 115200) / 2)
-#define newBaudrateVersion 1
+#define BIT_INTERVAL_THRESHOLD__us ((1e6 / 9600 + 1e6 / 115200) / 2)
+#define USE_NEAREST_BAUDRATE
 
 xQueueHandle interputQueue;
 
 static void IRAM_ATTR gpio_interrupt_handler(void *args)
 {
-    long long int timeDeliverd = esp_timer_get_time();
-    xQueueSendFromISR(interputQueue, &timeDeliverd, NULL);
+    long long int e = esp_timer_get_time();
+    xQueueSendFromISR(interputQueue, &e, NULL);
 }
 
 void uartStartDetectBaudrate()
@@ -21,24 +21,26 @@ void uartStartDetectBaudrate()
     gpio_set_level(PIN_DRQ, 0);
     //measure the baudrate
 
-    int64_t timestamps[bufferSize];
-    uint32_t baudrate = 0;
+    int64_t bitFlankTimestamps__us[bufferSize];
+    uint32_t bitFlankInterval__us = 0;
+    uint32_t smallestBitFlankInterval__us = 0;
+    uint32_t baudrate__b_s_1 = 0;
 
-    ESP_LOGI("Timer", "Started timer, time since boot: %lld us", timestamps[0] = esp_timer_get_time());
+    ESP_LOGI("Timer", "Started timer, time since boot: %lld us", bitFlankTimestamps__us[0] = esp_timer_get_time());
     gpio_set_direction(INPUT_PIN, GPIO_MODE_INPUT);
     
     static int detected = 0;
 
     ESP_LOGI("Baudrate", "calculating baudrate");
 
-    while (detected < bufferSize && (esp_timer_get_time() < (5000000 + timestamps[0]))) //10 seconds after boot
+    while (detected < bufferSize && (esp_timer_get_time() < (5000000 + bitFlankTimestamps__us[0]))) //10 seconds after boot
     {
-        long long int timeDeliverd;
-        if (xQueueReceive(interputQueue, &timeDeliverd, portTICK_PERIOD_MS))
+        long long int timeDelivered;
+        if (xQueueReceive(interputQueue, &timeDelivered, portTICK_PERIOD_MS))
         {
-            ESP_LOGI("Uartdata","found %lld us", timeDeliverd);
+            ESP_LOGI("Bit flank detected at","%lld us since boot", timeDelivered);
             detected++;
-            timestamps[detected] = timeDeliverd;
+            bitFlankTimestamps__us[detected] = timeDelivered;
         }
         else
         {
@@ -50,39 +52,41 @@ void uartStartDetectBaudrate()
 
     if(detected > 10)
     {
-        for (uint8_t i = 0; i < detected; i++)
+        smallestBitFlankInterval__us = bitFlankTimestamps__us[1]-bitFlankTimestamps__us[0];
+        ESP_LOGI("Bit flank interval found", "%d us", bitFlankInterval__us);
+        for (uint8_t i = 1; i < detected; i++)
         {
-            if(i < 1)
+            bitFlankInterval__us = bitFlankTimestamps__us[i+1]-bitFlankTimestamps__us[i];
+            ESP_LOGI("Bit flank interval found", "%d us", bitFlankInterval__us);
+            if(bitFlankInterval__us < smallestBitFlankInterval__us)
             {
-                baudrate = timestamps[i+1]-timestamps[i];
-            }
-            else
-            {
-                if(timestamps[i+1]-timestamps[i] < baudrate)
-                {
-                    baudrate = timestamps[i+1]-timestamps[i];
-                }
+                smallestBitFlankInterval__us = bitFlankInterval__us;
             }
         }
 
-        ESP_LOGI("Baudrate", "found %d us", baudrate);
+        ESP_LOGI("Smallest bit flank interval found", "%d us", smallestBitFlankInterval__us);
         
-        //comment abrc:7 #define newBaudrateVersion to use old version
-        #ifdef newBaudrateVersion
-            uint32_t candidateBaudRates[2] = {9600, 115200};
-            findNearestBaudRate(candidateBaudRates, baudrate);
+        #ifdef USE_NEAREST_BAUDRATE
+            uint32_t candidateBaudRates__b_s_1[3] = {9600, 115200, 0}; // must be 0-terminated!
+            baudrate__b_s_1 = findNearestBaudRate__b_s_1(candidateBaudRates__b_s_1, smallestBitFlankInterval__us);
+        ESP_LOGI("Baudrate found using find nearest:", "%d b/s", baudrate__b_s_1);
         #else
-            setBaudrate(baudrate);
+            baudrate__b_s_1 = decideBaudrate__b_s_1(smallestBitFlankInterval__us);
+        ESP_LOGI("Baudrate found using decide algorithm:", "%d b/s", baudrate__b_s_1);
         #endif
+        uart_set_baudrate(P1PORT_UART_NUM, baudrate__b_s_1);
+        ESP_LOGI("Baudrate set:", "%d b/s", (baudrate__b_s_1));
+
+
        
     }
     else
     {
-        ESP_LOGI("Baudrate", "No baudrate detected, time: %lld", esp_timer_get_time());
+        ESP_LOGI("Baudrate", "No baudrate detected until: %lld us since boot", esp_timer_get_time());
     }
-    uint32_t currentBaud = 0;
-    uart_get_baudrate(P1PORT_UART_NUM, &currentBaud);
-    ESP_LOGI("Baudrate", "%d in use", (currentBaud));
+    uint32_t current_b_s_1 = 0;
+    uart_get_baudrate(P1PORT_UART_NUM, &current_b_s_1);
+    ESP_LOGI("Baudrate get:", "%d b/s", (current_b_s_1));
     //Write DRQ pin low again (otherwise P1 port keeps transmitting every second);
     gpio_set_level(PIN_DRQ, 1);
 }
@@ -101,54 +105,55 @@ void abrInit()
     gpio_isr_handler_add(INPUT_PIN, gpio_interrupt_handler, (void *)INPUT_PIN);
 }
 
-void setBaudrate(uint32_t baudrate_us)
+uint32_t decideBaudrate__b_s_1(uint32_t smallestBitFlankInterval__us)
 {
-    uint32_t threshold__us = threshold_us;
+    uint32_t bitIntervalThreshold__us = BIT_INTERVAL_THRESHOLD__us;
 
-        if (baudrate_us < threshold__us)
-        {
-            baudrate_us = 115200;
-        }
-        else if (baudrate_us >= threshold__us && baudrate_us < 1000)//< 1ms
-        {
-            baudrate_us = 9600;
-        }
-        else
-        {
-            baudrate_us = 0;
-        }
-        ESP_LOGI("Baudrate", "calculated %d", baudrate_us);
-        uart_set_baudrate(P1PORT_UART_NUM, baudrate_us);
-}
-
-uint32_t calculateBitInterval(uint32_t baudrate)
-{
-    return 1e6 / baudrate;
-}
-
-void findNearestBaudRate(uint32_t * candidateBaudRates, uint32_t measuredBaudRate_us)
-{
-    uint32_t nearestBaudRate = candidateBaudRates[0];
-    int smallestDifference = (measuredBaudRate_us - calculateBitInterval(candidateBaudRates[0]));
-
-    for (uint8_t i = 0; i < sizeof(candidateBaudRates); i++)
+    if (smallestBitFlankInterval__us < bitIntervalThreshold__us)
     {
-        int difference = (measuredBaudRate_us - calculateBitInterval(candidateBaudRates[i]));
-        ESP_LOGI("Baudrate", "smalest difference = %d", smallestDifference);
-        ESP_LOGI("Baudrate", "difference = %d", difference);
-        if(smallestDifference < 0)
-        {
-            smallestDifference = smallestDifference * -1;
-        }
-        if (difference < smallestDifference)
-        {
-            nearestBaudRate = candidateBaudRates[i];
-            smallestDifference = difference;
-        }
-        ESP_LOGI("Baudrate", "nearest baudrate = %d", nearestBaudRate);
+        return 115200;
     }
+    else if (smallestBitFlankInterval__us >= bitIntervalThreshold__us && smallestBitFlankInterval__us < 1000)//< 1ms
+    {
+        return 9600;
+    }
+    else
+    {
+        return 0;
+    }
+}
 
-    ESP_LOGI("Baudrate", "calculated %d", nearestBaudRate);
+uint32_t calculateBitInterval__us(uint32_t baudrate__b_s_1)
+{
+    return 1e6 / baudrate__b_s_1;
+}
 
-    uart_set_baudrate(P1PORT_UART_NUM, nearestBaudRate);
+// candidateBaudRates__b_s_1 must be 0-terminated!
+uint32_t findNearestBaudRate__b_s_1(uint32_t * candidateBaudRates__b_s_1, uint32_t smallestBitFlankInterval__us)
+{
+
+    uint32_t nearest__b_s_1 = candidateBaudRates__b_s_1[0];
+    int smallestDifference__us = (smallestBitFlankInterval__us - calculateBitInterval__us(candidateBaudRates__b_s_1[0]));
+    if(smallestDifference__us < 0) {
+        smallestDifference__us = smallestDifference__us * -1;
+    }
+    ESP_LOGI("Baudrate", "%d b/s", candidateBaudRates__b_s_1[0]);
+    ESP_LOGI("Difference", "%d us\n", smallestDifference__us);
+
+    for (uint8_t i = 1; candidateBaudRates__b_s_1[i] != 0 ; i++)
+    {
+        int difference__us = (smallestBitFlankInterval__us - calculateBitInterval__us(candidateBaudRates__b_s_1[i]));
+        if(difference__us < 0) {
+            difference__us = difference__us * -1;
+        }
+        ESP_LOGI("Baudrate", "%d b/s", candidateBaudRates__b_s_1[i]);
+        ESP_LOGI("Difference", "%d us\n", difference__us);
+        if (difference__us < smallestDifference__us)
+        {
+            nearest__b_s_1 = candidateBaudRates__b_s_1[i];
+            smallestDifference__us = difference__us;
+        }
+    }
+    ESP_LOGI("Nearest baudrate:", "%d b/s", nearest__b_s_1);
+    return nearest__b_s_1;
 }
