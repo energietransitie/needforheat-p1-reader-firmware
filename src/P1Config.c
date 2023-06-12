0cd94038537f28402b9d8c2fc2993bb558199dcc
Joël <joel-weg@outlook.com>
2023-06-12 16:48:20 +0200

    9600 baud init
    dsmr 4 reading with 9600 baud init gave expected results

--- src/P1Config.c
+++ src/P1Config.c
@@ -11,7 +11,7 @@ void initP1UART_DSMR45() {
     
     //115200 baud, 8n1, no HW flow control
     uart_config_t uart_config = {
-        .baud_rate = 115200,
+        .baud_rate = 9600,
         .data_bits = UART_DATA_8_BITS,
         .parity = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
