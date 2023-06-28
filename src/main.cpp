4fabadd43e313c2b221340af3bc577c32cc5c2f9
hterhofte <h.ter.hofte@windesheim.nl>
2023-06-28 13:46:15 +0200

    fixed stress test schedule switch

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,11 @@ constexpr const char *DEVICE_TYPE_NAME = "twomes-p1-reader-firmware";
 
 extern "C" void app_main(void)
 {
-	Scheduler::AddTask(readP1Task, "uart_read_p1", 16384, NULL, 10, Scheduler::Interval::MINUTES_5);
+	#ifdef CONFIG_TWOMES_STRESS_TEST
+		Scheduler::AddTask(readP1Task, "uart_read_p1", 16384, NULL, 10, Scheduler::Interval::MINUTES_1);
+	#else
+		Scheduler::AddTask(readP1Task, "uart_read_p1", 16384, NULL, 10, Scheduler::Interval::MINUTES_10);
+	#endif
 
 	// Add the generic tasks to the scheduler,
 	// heartbeat, timesync and optionally presence detection and OTA updates.
