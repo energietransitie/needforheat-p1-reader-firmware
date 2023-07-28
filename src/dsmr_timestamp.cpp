83fbba94187a028fa5a15d25035dcf0f82099bb5
hterhofte <h.ter.hofte@windesheim.nl>
2023-07-28 09:39:28 +0200

    switc to using regular c functions

--- src/dsmr_timestamp.cpp
+++ src/dsmr_timestamp.cpp
@@ -3,7 +3,8 @@
 #include <esp_log.h>
 #include <iomanip>
 #include <sstream>
-#include <mbedtls/platform_time.h>
+#include <string>
+#include <ctime> // Include this header for standard time functions
 
 static int32_t lastTimestamp = TIME_UNKNOWN;       // Initialize to a default value
 static bool initializedLastTimeStamp = false;
@@ -93,122 +94,84 @@ void setLastTimestamp(int32_t newTimestamp)
 }
 
 time_t parseDsmrTimestamp(const std::string& timestampStr, time_t currentDeviceTime) {
+  const char* timeZone = "CET-1CEST,M3.5.0/2,M10.5.0/3"; // Europe/Amsterdam time zone
+  time_t unixTime = TIME_UNKNOWN;
   // Parse input timestamp string
-  // Set the time zone using the provided time zone string
   ESP_LOGD("parseDsmrTimestamp","dsmr timestamp: %s; current: %ld", timestampStr.c_str(), currentDeviceTime);
 
-  const char* timeZone = "CET-1CEST,M3.5.0/2,M10.5.0/3";  // Europe/Amsterdam time zone
-  mbedtls_platform_gmtime_time_t timeStruct;
-  mbedtls_platform_gmtime_init(&timeStruct);
+  std::string truncatedStr = timestampStr.substr(0, 12);
 
-  try {
-    // Create a truncated C-string with the first 12 characters (YYMMDDhhmmss)
-    char truncatedStr[13];
-    strncpy(truncatedStr, timestampStr.c_str(), 12);
-    truncatedStr[12] = '\0'; // Null-terminate the truncated C-string
-
-    // Parse the truncated C-string to extract the components
-    int year, month, day, hour, minute, second;
-    if (sscanf(truncatedStr, "%2d%2d%2d%2d%2d%2d", &year, &month, &day, &hour, &minute, &second) != 6) {
-        // Handle parsing error
-        ESP_LOGE("parseDsmrTimestamp", "Failed to parse timestamp");
-        return TIME_UNKNOWN; // Return an error value
-    }
-
-    // Set the parsed values in the timeStruct
-    timeStruct.year = year + 2000; // Assuming the timestamp is in YY format
-    timeStruct.mon = month;
-    timeStruct.day = day;
-    timeStruct.hour = hour;
-    timeStruct.min = minute;
-    timeStruct.sec = second;
-
-    // Use localtime_r() to check for ambiguity
-    mbedtls_platform_localtime_r(mbedtls_platform_gmtime(&timeStruct), &timeStruct);
-
-    // Check for ambiguity in the .dst value
-    if (timeStruct.dst == -1) {
-      // Handle ambiguous local time (YYMMDDhhmmss format)
-
-      if (timestampStr.length() == 13) {
-          // Check the 'X' character to disambiguate
-          char dstFlag = timestampStr[12]; // 'X' character indicating DST status
-          if (dstFlag == 'S') {
-              timeStruct.dst = MBEDTLS_PLATFORM_TIME_DST_ON;
-          } else if (dstFlag == 'W') {
-              timeStruct.dst = MBEDTLS_PLATFORM_TIME_DST_OFF;
-          } else {
-              ESP_LOGE("parseDsmrTimestamp", "unexpected last character");
-              return TIME_UNKNOWN; // Return an error value
-          }
-      } else {
-        // Handle ambiguous hour & format: YYMMDDhhmmss (DSMR2/DSMR3)
-        ESP_LOGE("parseDsmrTimestamp", "Ambiguous local time for: %s", truncatedStr);
-
-        // Need to look at persistently stored timestamps to decide.
-        // In very rare cases, we have to rely even on currentDeviceTime given in parameter
-        int lastTimestamp = getLastTimestamp();
-        if (lastTimestamp == -1 && currentDeviceTime != 0) {
-            // Edge case: no lastTimestamp stored yet in nvs; the only place where we use currentDeviceTime
-            std::tm* tmCurrentDeviceTimeUTC = std::gmtime(&currentDeviceTime);
-            if (tmCurrentDeviceTimeUTC->tm_hour == 0) {
-                // The ambiguous hour is the first occurrence of 2-3 am
-                timeStruct.dst = MBEDTLS_PLATFORM_TIME_DST_ON;
-                if (timeStruct.hour == 2) {
-                    int timestampInSeconds = timeStruct.hour * 3600 + timeStruct.min * 60 + timeStruct.sec;
-                    setLastTimestamp(timestampInSeconds);
-                }
-            } else {
-                timeStruct.dst = MBEDTLS_PLATFORM_TIME_DST_OFF;
-            }
-        } else {
-            // Ambiguous hour, set timeStruct.dst based on the value of lastTimestamp
-            int timeInSeconds = timeStruct.tm_hour * 3600 + timeStruct.tm_min * 60 + timeStruct.tm_sec;
-            if (timeInSeconds < lastTimestamp) {
-                // The ambiguous hour is the second occurrence of 2-3 am, so no DST
-                timeStruct.dst = MBEDTLS_PLATFORM_TIME_DST_OFF;
-            } else {
-                timeStruct.dst = MBEDTLS_PLATFORM_TIME_DST_ON;
-                int timestampInSeconds = timeStruct.hour * 3600 + timeStruct.min * 60 + timeStruct.sec;
-                setLastTimestamp(timestampInSeconds);
-            }
-        }
-      }
-  } else if (timestampStr.length() == 13) {
-      // Check if the parsed DST status matches the 'X' character to ensure consistency
-      char dstFlag = timestampStr[12]; // 'X' character indicating DST status
-      if ((dstFlag == 'S' && timeStruct.dst == MBEDTLS_PLATFORM_TIME_DST_OFF) || (dstFlag == 'W' && timeStruct.dst == MBEDTLS_PLATFORM_TIME_DST_ON)) {
-          ESP_LOGW("parseDsmrTimestamp", "DST status mismatch between localtime_r and X character");
-          // Correct the DST status based on 'X' character
-      char dstFlag = timestampStr[12]; // 'X' character indicating DST status
-      if (dstFlag == 'S') {
-        timeStruct.dst = MBEDTLS_PLATFORM_TIME_DST_ON;
-      } else if (dstFlag  == 'W') {
-        timeStruct.dst = MBEDTLS_PLATFORM_TIME_DST_OFF;
-      } else {
-        // Handle unexpected last character
-      ESP_LOGE("parseDsmrTimestamp","unexpected last character");
-        return TIME_UNKNOWN; // Return an error value
-      }
-  }
-} catch (const std::exception& e) {
-  // Handle parsing or other exceptions if needed
-  ESP_LOGE("parseDsmrTimestamp", "exception occurred: %s", e.what());
-  return TIME_UNKNOWN; // Return an error value
-} 
-
+  // Convert the truncated timestamp string to a time structure
+  struct tm timeStruct;
+  if (sscanf(truncatedStr.c_str(), "%2d%2d%2d%2d%2d%2d", &timeStruct.tm_year, &timeStruct.tm_mon,
+              &timeStruct.tm_mday, &timeStruct.tm_hour, &timeStruct.tm_min, &timeStruct.tm_sec) != 6) {
     // Handle invalid timestamp format or parsing error
     ESP_LOGE("parseDsmrTimestamp"," invalid timestamp format or parsing error for: %s", truncatedStr.c_str());
     return TIME_UNKNOWN; // Return an error value
-    // Handle other exceptions if needed
-    ESP_LOGE("parseDsmrTimestamp","other exceptions for: %s", truncatedStr.c_str());
-    return TIME_UNKNOWN; // Return an error value
+
+      return TIME_UNKNOWN; // Return an error value
   }
 
+  // Convert the year to tm_year format (since tm_year represents years since 1900)
+  timeStruct.tm_year += 100; // Assuming the year format is YY (e.g., 23 for 2023)
 
-  time_t unixTime = boost::posix_time::to_time_t(ldt);
-  ESP_LOGD("parseDsmrTimestamp","unix time: %ld", unixTime);
+  // Set the time zone using the provided time zone string
+  setenv("TZ", timeZone, 1); // 1 for overwrite
+  // Use localtime_r() to check for ambiguity
+  localtime_r(&unixTime, &timeStruct);
 
+  // Check for ambiguity in the .dst value
+  if (timeStruct.tm_isdst == -1) {
+    // Handle ambiguous local time (YYMMDDhhmmss format)
+
+    if (timestampStr.length() == 13) {
+        // Check the 'X' character to disambiguate
+        char dstFlag = timestampStr[12]; // 'X' character indicating DST status
+        if (dstFlag == 'S') {
+            timeStruct.tm_isdst = 1;
+        } else if (dstFlag == 'W') {
+            timeStruct.tm_isdst = 0;
+        } else {
+            ESP_LOGE("parseDsmrTimestamp", "unexpected last character");
+            return TIME_UNKNOWN; // Return an error value
+        }
+    } else {
+      // Handle ambiguous hour & format: YYMMDDhhmmss (DSMR2/DSMR3)
+      ESP_LOGE("parseDsmrTimestamp", "Ambiguous local time for: %s", truncatedStr.c_str());
+
+      // Need to look at persistently stored timestamps to decide.
+      // In very rare cases, we have to rely even on currentDeviceTime given in parameter
+      int lastTimestamp = getLastTimestamp();
+      if (lastTimestamp == -1 && currentDeviceTime != 0) {
+        // Edge case: no lastTimestamp stored yet in nvs; the only place where we use currentDeviceTime
+        std::tm* tmCurrentDeviceTimeUTC = std::gmtime(&currentDeviceTime);
+        if (tmCurrentDeviceTimeUTC->tm_hour == 0) {
+            // The ambiguous hour is the first occurrence of 2-3 am
+            timeStruct.tm_isdst = 1;
+            if (timeStruct.tm_hour == 2) {
+                int timestampInSeconds = timeStruct.tm_hour * 3600 + timeStruct.tm_min * 60 + timeStruct.tm_sec;
+                setLastTimestamp(timestampInSeconds);
+            }
+        } else {
+            timeStruct.tm_isdst = 0;
+        }
+      } else {
+        // Ambiguous hour, set timeStruct.tm_isdst based on the value of lastTimestamp
+        int timeInSeconds = timeStruct.tm_hour * 3600 + timeStruct.tm_min * 60 + timeStruct.tm_sec;
+        if (timeInSeconds < lastTimestamp) {
+            // The ambiguous hour is the second occurrence of 2-3 am, so no DST
+            timeStruct.tm_isdst = 0;
+        } else {
+            timeStruct.tm_isdst = 1;
+            int timestampInSeconds = timeStruct.tm_hour * 3600 + timeStruct.tm_min * 60 + timeStruct.tm_sec;
+            setLastTimestamp(timestampInSeconds);
+        }
+      }
+    }
+  
+  }
+  // Convert the struct tm to a Unix timestamp (Unix time)
+  unixTime = mktime(&timeStruct);
   // Return the Unix time
   return unixTime;
 }
