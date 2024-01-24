1796263b1102495ff21a195fae45e7034936398a
hterhofte <h.ter.hofte@windesheim.nl>
2024-01-24 13:46:13 +0100

    do not upload incorrect p1 messages

--- src/p1.cpp
+++ src/p1.cpp
@@ -72,6 +72,8 @@ void readP1Task(void *taskInfo)
 	{
 		ESP_LOGI("P1", "incorrect p1 message");
 		GenericESP32Firmware::BlinkLED(RED_LED_D1_ERROR, 2);
+		// Exit the function early since there is no good data
+        return;
 	}
 
 	time_t e_time_t = TIME_UNKNOWN;
